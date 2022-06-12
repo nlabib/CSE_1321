8552f1f1ead909f095527989b5b293d50a0aa70c
nasimullabib <nlabib@students.kennesaw.edu>
2022-06-11 22:10:39 -0400

    Testing

---
+++ Lab4/Lab4C.cs
@@ -0,0 +1,58 @@
+/*
+Class: CSE 1321L
+Section: w2
+Term: Summer 2022
+Instructor: Kavitha Muppa
+Name: Nasimul Labib
+Lab#: 4B
+*/
+
+using System;
+
+public class Lab4B
+{
+	public static void Main(string[] args)
+	{
+
+        Console.Write("Welcome!");
+        Console.WriteLine("Please input a number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("");
+        Console.WriteLine("What would you like to do to this number: ");
+        Console.WriteLine("0- Get the additive inverse of the number");
+        Console.WriteLine("1- Get the reciprocal of the number");
+        Console.WriteLine("2- Square the number");
+        Console.WriteLine("3- Cube the number");
+        Console.WriteLine("4- Exit the program");
+      
+        // Additive inverse 
+        int additive = (2 * num) - num ;
+        // Reciprocal of the number
+
+        // Squre the number
+
+        // Cube the number
+
+
+
+        
+        Console.WriteLine(" ");// Input from the user
+        int command = Convert.ToInt32(Console.ReadLine()); // save input
+
+
+
+        // [The loop]
+
+        switch (command){
+            case 0:
+                
+        }
+
+
+
+
+
+
+    }
+    }
