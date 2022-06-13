a28f24218b3176b3c75323eaaffca170428b8688
nasimullabib <nlabib@students.kennesaw.edu>
2022-06-13 02:40:34 -0400

    fixing errors

--- Lab4/Lab4A.cs
+++ Lab4/Lab4A.cs
@@ -7,74 +7,74 @@ Name: Nasimul Labib
 Lab#: 4A
 */
 
-using System;
+usingrade System;
 
 public class Lab4A
 {
-	public static void Main(string[] args)
+	public static void Main(stringrade[] argrades)
 	{
 
 		Console.Write("Enter the score of your exam: ");
-		float grade = float.Parse(Console.ReadLine());
+		float graderade = float.Parse(Console.ReadLine());
 		
-		int g = (int) Math.Round(grade, 0);
+		int grade = (int) Math.Round(graderade, 0);
 
-		if (g >= 98 && g <= 100)
+		if (grade >= 98 && grade <= 100)
 		{
-			Console.WriteLine("Letter g is: A+");
+			Console.WriteLine("Letter graderade is: A+");
 		}
-		else if (g >= 95 && g <= 97)
+		else if (grade >= 95 && grade <= 97)
 		{
-			Console.WriteLine("Letter g is: A");
+			Console.WriteLine("Letter grade is: A");
 		}
-		else if (g >= 92 && g <= 94)
+		else if (grade >= 92 && grade <= 94)
 		{
-			Console.WriteLine("Letter g is: A-");
+			Console.WriteLine("Letter grade is: A-");
 		}
-		else if (g >= 89 && g <= 91)
+		else if (grade >= 89 && grade <= 91)
 		{
-			Console.WriteLine("Letter g is: B+");
-
-		}
-		else if (g >= 86 && g <= 88)
-		{
-			Console.WriteLine("Letter g is: B");
+			Console.WriteLine("Letter grade is: B+");
 
 		}
-		else if (g >= 83 && g <= 85)
+		else if (grade >= 86 && grade <= 88)
 		{
-			Console.WriteLine("Letter g is: B-");
+			Console.WriteLine("Letter grade is: B");
 
 		}
-		else if (g >= 80 && g <= 82)
+		else if (grade >= 83 && grade <= 85)
 		{
-			Console.WriteLine("Letter g is: C+");
+			Console.WriteLine("Letter grade is: B-");
 
 		}
-		else if (g >= 77 && g <= 79)
+		else if (grade >= 80 && grade <= 82)
 		{
-			Console.WriteLine("Letter g is: C");
+			Console.WriteLine("Letter grade is: C+");
 
 		}
-		else if (g >= 74 && g <= 76)
+		else if (grade >= 77 && grade <= 79)
 		{
-			Console.WriteLine("Letter g is: C-");
+			Console.WriteLine("Letter grade is: C");
+
 		}
-		else if (g >= 71 && g <= 73)
+		else if (grade >= 74 && grade <= 76)
 		{
-			Console.WriteLine("Letter g is: D+");
+			Console.WriteLine("Letter grade is: C-");
 		}
-		else if (g >= 68 && g <= 70)
+		else if (grade >= 71 && grade <= 73)
 		{
-			Console.WriteLine("Letter g is: D");
+			Console.WriteLine("Letter grade is: D+");
 		}
-		else if (g >= 65 && g <= 67)
+		else if (grade >= 68 && grade <= 70)
 		{
-			Console.WriteLine("Letter g is: D-");
+			Console.WriteLine("Letter graderade is: D");
+		}
+		else if (grade >= 65 && grade <= 67)
+		{
+			Console.WriteLine("Letter grade is: D-");
 		}
 		else 
 		{
-			Console.WriteLine("Letter g is: F");
+			Console.WriteLine("Letter grade is: F");
 		}
 
 	}
--- Lab4/Lab4C.cs
+++ Lab4/Lab4C.cs
@@ -4,7 +4,7 @@ Section: w2
 Term: Summer 2022
 Instructor: Kavitha Muppa
 Name: Nasimul Labib
-Lab#: 4B
+Lab#: 4C
 */
 
 using System;
@@ -17,7 +17,7 @@ public class Lab4B
         Console.Write("Welcome!");
         Console.WriteLine("Please input a number: ");
         int num = Convert.ToInt32(Console.ReadLine());
-
+		/*
         Console.WriteLine("");
         Console.WriteLine("What would you like to do to this number: ");
         Console.WriteLine("0- Get the additive inverse of the number");
@@ -26,33 +26,95 @@ public class Lab4B
         Console.WriteLine("3- Cube the number");
         Console.WriteLine("4- Exit the program");
       
+       	*/ 
+
+        // [Calculations]
         // Additive inverse 
-        int additive = (2 * num) - num ;
+        int additive = num - (2 * num);
         // Reciprocal of the number
-
+        string reciprocal = (string) ("1 / "+ num);
         // Squre the number
-
+        int square = num * num;
         // Cube the number
+        int cube = num * num * num;
 
 
+      
 
-        
+     
         Console.WriteLine(" ");// Input from the user
         int command = Convert.ToInt32(Console.ReadLine()); // save input
+		/*
+        if (command != 4){
+
+        
+       	 	Console.WriteLine("What would you like to do to this number: ");
+        	Console.WriteLine("0- Get the additive inverse of the number");
+        	Console.WriteLine("1- Get the reciprocal of the number");
+        	Console.WriteLine("2- Square the number");
+        	Console.WriteLine("3- Cube the number");
+        	Console.WriteLine("4- Exit the program");
+
+        	/*
+        	if (command == 0){
+        		Console.WriteLine("The additive inverse of " + num + " is " + additive);
+        	}
+        	else if (command == 1){
+        		Console.WriteLine("The reciprocal of " + num + " is " + reciprocal);
+        	}
+        	else if (command == 2){
+        		Console.WriteLine("The square of " + num + " is " + square);
+        	}
+        	else if (command == 3){
+        		Console.WriteLine("The cube " + num + " is " + cube);
+        	}
+        	
+        	// [The loop]
 
 
-
-        // [The loop]
-
-        switch (command){
+        } 
+		*/
+		do {
+			switch (command){
             case 0:
-                
-        }
+            	Console.WriteLine("The additive inverse of " + num + " is " + additive);
+            	break;
+            case 1:
+            	Console.WriteLine("The reciprocal of " + num + " is " + reciprocal);
+            	break;
+            case 2:
+            	Console.WriteLine("The square of " + num + " is " + square);
+            	break;
+            case 3:
+            	Console.WriteLine("The cube " + num + " is " + cube);
+            	break;
+            case 4:
+            	Console.WriteLine("Thank you, goodbye!");
+            	break;   
+            default:
+            	Console.WriteLine("What would you like to do to this number: ");
+	        	Console.WriteLine("0- Get the additive inverse of the number");
+	        	Console.WriteLine("1- Get the reciprocal of the number");
+	        	Console.WriteLine("2- Square the number");
+	        	Console.WriteLine("3- Cube the number");
+	        	Console.WriteLine("4- Exit the program");
+            	Console.WriteLine("Invalid input, please try again!"); 
+            	break;
+		}
+		while (command != 4)
+        	
+        // if statement
 
+		
 
+        
+       }
+   		}
+    } // main 
+    //class
 
+        
+        
 
-
-
-    }
-    }
+        
+  
