b104137d3f4d7944d2f07a1daaa9a5ea3708dc0c
nasimullabib <nlabib@students.kennesaw.edu>
2022-06-14 23:45:14 -0400

    Lab 5 Done

---
+++ Lab5/Lab5C.cs
@@ -0,0 +1,54 @@
+/*
+Class: CSE 1321L
+Section: w2
+Term: Summer 2022
+Instructor: Kavitha Muppa
+Name: Nasimul Labib
+Lab#: 5C
+*/
+
+using System;
+
+public class Lab5C
+{
+   public static void Main(string[] args)
+   {
+
+       Console.Write("Enter x: ");
+       int x = Convert.ToInt32(Console.ReadLine());
+
+       Console.Write("Enter y: ");
+       int y = Convert.ToInt32(Console.ReadLine());
+
+
+      if (x == 0 && y == 0){
+         Console.WriteLine("This point is the origin.");
+
+      }
+      else if(x > 0 && y > 0){
+         Console.WriteLine("This is in the first quadrant.");
+      }
+      else if(x < 0 && y > 0){
+         Console.WriteLine("This is in the second quadrant.");
+      }
+      else if (x < 0 && y < 0){
+         Console.WriteLine("This is in the third quadrant.");
+      }
+      else if (x > 0 && y < 0){
+         Console.WriteLine("This is in the fourth quadrant.");
+      }
+      else if (x == 0){
+         Console.WriteLine("This is on the x axis.");
+      }
+      else if (y == 0){
+         Console.WriteLine("This is on the y axis.");
+      }
+      else
+      {
+         Console.WriteLine("Number error.");
+      }
+
+
+    }
+       }
+   
