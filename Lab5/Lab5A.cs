60aa7f15fbb60a2a3a59a41f4720e95dd9180a91
nasimullabib <nlabib@students.kennesaw.edu>
2022-06-14 20:38:23 -0400

    Lab5A done

--- Lab5/Lab5A.cs
+++ Lab5/Lab5A.cs
@@ -8,13 +8,27 @@ Lab#: 4C
 */
 
 using System;
-
-public class Lab4C
-{
-   public static void Main(string[] args)
+public class Lab5A{
+public static void Main(string[] args)
    {
 
+         Console.WriteLine("Do you have a driving permit (Y/N)?");
+         char first = Convert.ToChar(Console.ReadLine());
 
-            // ok no clue what the problem is
+      
+         if(first.Equals('Y') || first.Equals('y')){
+            Console.WriteLine("Do you have a commercial driving license (Y/N)? ");
+            char second = Convert.ToChar(Console.ReadLine());
+            if (second.Equals('Y') || second.Equals('y')){
+               Console.WriteLine("Congratulations! You can purchase a vehicle, let’s start talking options!");
+            }
+            else{
+               Console.WriteLine("Commercial driving license is a prerequisite to purchase a vehicle!");
+            }
+            
+         }
+       else{
+          Console.WriteLine("Driving permit is a prerequisite to purchase a vehicle! ");
        }
-   
+}
+    }
