0d800ea9e25325c273266faf1684396674e86b43
nasimullabib <nlabib@students.kennesaw.edu>
2022-06-14 21:51:03 -0400

    5B

---
+++ Lab5/Lab5B.cs
@@ -0,0 +1,21 @@
+/*
+Class: CSE 1321L
+Section: w2
+Term: Summer 2022
+Instructor: Kavitha Muppa
+Name: Nasimul Labib
+Lab#: 5B
+*/
+
+using System;
+
+public class Lab4B
+{
+   public static void Main(string[] args)
+   {
+
+       
+
+    }
+       }
+   
