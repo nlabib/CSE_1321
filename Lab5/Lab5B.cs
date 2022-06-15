03802c42143c51e2c1a7c273875e857f769599c4
nasimullabib <nlabib@students.kennesaw.edu>
2022-06-14 23:14:02 -0400

    Lab5B done

--- Lab5/Lab5B.cs
+++ Lab5/Lab5B.cs
@@ -9,12 +9,36 @@ Lab#: 5B
 
 using System;
 
-public class Lab4B
+public class Lab5B
 {
    public static void Main(string[] args)
    {
 
-       
+       Console.WriteLine("Enter an integer: ");
+       int num = Convert.ToInt32(Console.ReadLine());
+
+       int d2 = num % 2;
+
+       int d3 = num % 3;
+
+       int d5 = num % 5;
+
+       if(d2 == 0){
+         Console.WriteLine("This number is divisible by 2");
+
+       }
+       else if(d3 == 0){
+         Console.WriteLine("This number is divisible by 3");
+       }
+       else if (d5 == 0){
+         Console.WriteLine("This number is divisible by 5");
+       }
+
+       else{
+         Console.WriteLine("This number is undetermined");
+       }
+
+
 
     }
        }
