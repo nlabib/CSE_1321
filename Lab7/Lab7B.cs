727c591270a1451f6439c80255f321bf68a8b127
nasimullabib <nlabib@students.kennesaw.edu>
2022-06-26 12:26:06 -0400

    Lab7 Done

--- Lab7/Lab7B.cs
+++ Lab7/Lab7B.cs
@@ -18,10 +18,10 @@ public class Lab4B
       Console.Write("Please enter a value for the size: ");
       int input = Convert.ToInt32(Console.ReadLine());
 
-      Console.WriteLine("This is the requested " + input + "x" + input + " box:");
+      Console.WriteLine("This is the requested " + input + "x" + input + " right-triangle:");
 
       for(int i=0; i < input; ++i){
-         for(int j = 0; j < input; ++j){
+         for(int j = 0; j <= i; ++j){
             Console.Write("*");
          }
          Console.WriteLine("");
---
+++ Lab7/Lab7C.cs
@@ -0,0 +1,35 @@
+/*
+Class: CSE 1321L
+Section: w2
+Term: Summer 2022
+Instructor: Kavitha Muppa
+Name: Nasimul Labib
+Lab#: 4C
+*/
+
+using System;
+
+public class Lab4B
+{
+   public static void Main(string[] args)
+   {
+         Console.Write("Please enter a value for the size: ");
+         int input = Convert.ToInt32(Console.ReadLine());
+
+         Console.WriteLine("This is the requested " + input + "x" + input + " right-triangle:");
+
+         for(int i=0; i < input; i++){
+
+            for(int j=2*(input-i); j>=0; j--){
+               Console.Write(" ");
+            }
+
+            for(int j = 0; j <= i; j++){
+               Console.Write(" *");
+            }
+            Console.WriteLine("");
+         }
+
+    }
+       }
+   
